3df15eaf1d1ac855d3c53a01c0c1aacc5583f083
Lars Brinkhoff <lars@nocrew.org>
2018-09-12 08:26:39 +0200

    For CI, install some dependencies for libsdl2.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -11,7 +11,8 @@ install_linux() {
     sudo apt-get install -my git make gcc libncurses-dev autoconf
     case "$EMULATOR" in
         simh) sudo apt-get install -y simh;;
-        sims) sudo apt-get install -y libx11-dev libxt-dev libsdl2-dev;;
+        sims) sudo apt-get install -y libegl1-mesa-dev libgles2-mesa-dev
+              sudo apt-get install -y libx11-dev libxt-dev libsdl2-dev;;
     esac
 }
 
